e8a29682f61c6bccda956e0ee8c89c0cffcc17ec
SlesarevaMA <slesareva.m.a@yandex.ru>
2022-08-20 19:24:48 +0300

    Initial Commit

---
+++ EcommerceConcept/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EcommerceConcept
+//
+//  Created by Margarita Slesareva on 20.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
