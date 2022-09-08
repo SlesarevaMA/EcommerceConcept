d2f37da0cabac8b40993180e7b287ec2191e84a8
Margarita Slesareva <slesareva.m.a@yandex.ru>
2022-09-08 14:42:30 +0300

    configure SelectCategoryViewCell

--- EcommerceConcept/Presentation/Main/Cells/SelectCategoryViewCell.swift
+++ EcommerceConcept/Presentation/Main/Cells/SelectCategoryViewCell.swift
@@ -7,6 +7,15 @@
 
 import UIKit
 
+private enum Metrics {
+    static let categoryLabelIndent: CGFloat = 8
+    static let verticalSpacing: CGFloat = 7
+    
+    static let categoryLabelFont: UIFont = .systemFont(ofSize: 12)
+    
+    static let isSelectedColor: UIColor = .init(hex: 0xFF6E4E)
+}
+
 final class SelectCategoryViewCell: UICollectionViewCell, Identifiable {
     
     private let imageView = UIImageView()
@@ -20,6 +29,16 @@ final class SelectCategoryViewCell: UICollectionViewCell, Identifiable {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(with model: SelectCategoryCellViewModel) {
+        imageView.image = model.image
+        categoryLabel.text = model.category
+    }
+    
+    func isSelected() {
+        imageView.backgroundColor = Metrics.isSelectedColor
+        categoryLabel.textColor = Metrics.isSelectedColor
     }
     
     private func configure() {
@@ -36,12 +55,21 @@ final class SelectCategoryViewCell: UICollectionViewCell, Identifiable {
         NSLayoutConstraint.activate([
             imageView.widthAnchor.constraint(equalTo: imageView.heightAnchor),
             imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            imageView.topAnchor.constraint(equalTo: contentView.topAnchor)
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            
+            categoryLabel.leadingAnchor.constraint(
+                equalTo: contentView.leadingAnchor,
+                constant: Metrics.categoryLabelIndent
+            ),
+            categoryLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: Metrics.verticalSpacing)
         ])
-
     }
     
     private func configureSubviews() {
+        imageView.layer.cornerRadius = imageView.frame.height / 2
         
+        categoryLabel.font = Metrics.categoryLabelFont
+        categoryLabel.textAlignment = .center
     }
 }
---
+++ EcommerceConcept/Presentation/Main/ViewModels/SelectCategoryCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SelectCategoryCellViewModel.swift
+//  EcommerceConcept
+//
+//  Created by Margarita Slesareva on 08.09.2022.
+//
+
+import UIKit
+
+struct SelectCategoryCellViewModel {
+    let image: UIImage?
+    let category: String
+}
